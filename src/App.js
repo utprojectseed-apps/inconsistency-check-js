3db953883eb0af80993d8a7b3b5ef09a9945288f
Viet-L <vietle542521@gmail.com>
2024-05-27 09:18:42 -0500

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { NavLink, Routes, Route, useLocation } from 'react-router-dom';
+import Home from "./pages/home"
+import About from "./pages/about"
+import Contact from "./pages/contact"
 
-function App() {
+const App = () => (
+  <div className='app'>
+    <h1>React Router Demo</h1>
+    <Main />
+  </div>
+);
+
+const Navigation = () => (
+  <nav>
+    <ul>
+      <li><NavLink to='/'>Home</NavLink></li>
+      <li><NavLink to='/about'>About</NavLink></li>
+      <li><NavLink to='/contact'>Contact</NavLink></li>
+    </ul>
+  </nav>
+);
+
+const Main = () => {
+  const location = useLocation();
+  const hideNavigation = false
+  // hideNavigation = location.pathname !== '/about'; // Define the condition to hide navigation
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {!hideNavigation && <Navigation />}
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+      </Routes>
+    </>
+);
 }
 
 export default App;
+
---
+++ src/pages/home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className='home'>
+            <h1>Welcome to my portfolio website</h1>
+            <p> Feel free to browse around and learn more about me.</p>
+        </div>
+    )
+}
