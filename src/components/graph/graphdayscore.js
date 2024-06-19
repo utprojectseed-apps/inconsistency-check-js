53bae9ae6f5383804de146cffcf00647824df6da
Viet-L <vietle542521@gmail.com>
2024-06-19 08:45:14 -0500

    Clean up

--- src/components/graph/graphdayscore.js
+++ src/components/graph/graphdayscore.js
@@ -1,5 +1,6 @@
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ReferenceLine, ResponsiveContainer } from 'recharts';
 export default function GraphDayScore( {participant} ) {
+    if(participant.getId() <= 7789) { return null }
     const rawData = participant.game.getScores()
     const data = [] 
     for (let i = 0; i < rawData.length; i++) {
@@ -28,7 +29,7 @@ export default function GraphDayScore( {participant} ) {
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis dataKey="name" />
                     <YAxis domain={[-1, 1]}/>
-                    <ReferenceLine y={0} label="Max" stroke="red" strokeDasharray="3 3" />
+                    <ReferenceLine y={0} stroke="red" strokeDasharray="3 3" />
                     <Tooltip />
                     <Legend />
                     <Line type="monotone" dataKey="score" stroke="#8884d8" activeDot={{ r: 8 }} />
@@ -37,26 +38,3 @@ export default function GraphDayScore( {participant} ) {
         </div>
     )
 }
-
-/*
-<ResponsiveContainer width="100%" height={400}>
-                <LineChart
-                    width={500}
-                    height={300}
-                    data={data}
-                    margin={{
-                    top: 5,
-                    right: 30,
-                    left: 20,
-                    bottom: 5,
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-                    <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-                </LineChart>
-            </ResponsiveContainer>*/
