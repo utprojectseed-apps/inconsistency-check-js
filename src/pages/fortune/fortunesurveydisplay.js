5553d24a30e6b813607d100a7b1e9c420767d75b
Viet-L <vietle542521@gmail.com>
2024-07-06 08:14:02 -0500

    Read user input form

--- src/pages/fortune/fortunesurveydisplay.js
+++ src/pages/fortune/fortunesurveydisplay.js
@@ -1,11 +1,27 @@
 import FortuneSurvey from "../../survey/fortunesurvey";
+import CSVReader from "../../components/csvread";
+import React, { useEffect, useReducer } from "react";
 
 export default function FortuneSurveyDisplay() {
+    const [data, setData] = React.useState(undefined)
+    const [, forceUpdate] = useReducer(x => x + 1, 0);
+    const [errorMessage, setErrorMessage] = React.useState(undefined)   
+    const handleUpload = d => {
+        setData(d)
+    }
+    useEffect(() => {
+        if(data !== undefined) {
+            console.log(data)
+            forceUpdate()
+        }
+    }, [data])
+
     const survey = new FortuneSurvey([]);
     survey.readFortuneCSV()
     return (
         <div className='survey'>
             <h1>TODO Survey code</h1>
+            <div className='no-print'><CSVReader parentCallback={handleUpload} gameId = "fortune"/></div>
         </div>
     )
 }
