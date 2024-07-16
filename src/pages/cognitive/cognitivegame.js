df7af72bcb470461279cf00c7a42d40eb605602a
Emely Diaz <emely18diaz@gmail.com>
2024-07-16 08:45:10 -0500

    Update cognitivegame.js
    following fd implementation

--- src/pages/cognitive/cognitivegame.js
+++ src/pages/cognitive/cognitivegame.js
@@ -4,6 +4,7 @@ import React, { useEffect, useRef, useReducer } from "react";
 import * as dfd from 'danfojs';
 import ParticipantList from "../../game_data/participants";
 import GamesFullReport from "../../components/gamesfullreport";
+import CheckboxesTags from "../../components/checkboxestags";
 
 export default function CognitiveGame() {
     const [bdsData, setBdsData] = React.useState(undefined)
@@ -11,7 +12,8 @@ export default function CognitiveGame() {
     const [csData, setCsData] = React.useState(undefined)
     const [, forceUpdate] = useReducer(x => x + 1, 0);
     const participantList = useRef(null)
-    const [errorMessage, setErrorMessage] = React.useState(undefined)   
+    const [errorMessage, setErrorMessage] = React.useState(undefined)
+    const [selectedIds, setSelectedIds] = React.useState(undefined)
     const handleUpload = (d, game) => {
         switch(game) {
             case "bds":
@@ -26,6 +28,9 @@ export default function CognitiveGame() {
             default:
                 throw new Error("Unknown game: " + game);
         }
+    }
+    const handleSelected = d => {
+        setSelectedIds(d)
     }
     // useEffect(() => {
     //     if(csData != undefined) {
@@ -91,12 +96,17 @@ useEffect(() => {
     }, [bdsData, simonData])
     return (
         <div className='games'>
-            <h1>Enter data NOT IMPLEMENTED YET Order:(BDS, SIMON, CS)</h1>
-            <CSVReader parentCallback={handleUpload} gameId="bds" key="bds"/>
-            <CSVReader parentCallback={handleUpload} gameId="simon" key="simon"/>
-            <CSVReader parentCallback={handleUpload} gameId="cs" key="cs"/>
+            <h1 className='no-print'>Enter data NOT IMPLEMENTED FULLY YET Order:(BDS, SIMON, CS)</h1>
+            <div className='no-print'><CSVReader parentCallback={handleUpload} gameId="bds"/></div>
+            <div className='no-print'><CSVReader parentCallback={handleUpload} gameId="simon"/></div>
+            <div className='no-print'><CSVReader parentCallback={handleUpload} gameId="cs"/></div>
             {errorMessage && <h2>{errorMessage}</h2>}
-            {!errorMessage && <GamesFullReport participantList={participantList.current}/>}
+            {participantList.current !== null && 
+                <div className='no-print'>
+                    <CheckboxesTags className="no-print" ids={participantList.current.getIds()} parentCallback={handleSelected}/>
+                </div>
+            }
+            {!errorMessage && <GamesFullReport participantList={participantList.current} activeIds={selectedIds}/>}
         </div>
     )
 }
