d0eeedd432c6060c34e40ed2ee1c01fbb17f501e
Viet-L <vietle542521@gmail.com>
2024-07-27 21:53:31 -0500

    Update completion

--- src/survey/surveyparticipant.js
+++ src/survey/surveyparticipant.js
@@ -5,6 +5,10 @@ export default class SurveyParticipant {
         this.dataDict = dataDict
     }
 
+    static getDays() {
+        return 14
+    }
+
     toString() {
         let firstName = "FIRST"
         let lastName = "LAST"
@@ -23,14 +27,90 @@ export default class SurveyParticipant {
     }
 
     #findDailyPercent() {
-        console.log(this.#branchMet('t1cvib01'))
-        // console.log(this.data.values)
-        // console.log(this.data.columns)
+        let answerArray = Array(SurveyParticipant.getDays()).fill().map(() => [])
+        let columnArray = Array(SurveyParticipant.getDays()).fill().map(() => [])
+        // find start and end of each day
+        let dayStartEnd = []
+        let days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+        for(let i = 1; i <= SurveyParticipant.getDays(); ++i) {
+            let startCol = `day_${i}_${days[(i - 1) % 7]}_daily_survey_timestamp`
+            let endCol = `day_${i}_${days[(i - 1) % 7]}_daily_survey_complete`
+            let startIndex = this.data.columns.indexOf(startCol)
+            let endIndex = this.data.columns.indexOf(endCol)
+            dayStartEnd.push([startIndex, endIndex])
+        }
+        // loop through each day and fill in arrays based on day
+        for(let i = 1; i <= SurveyParticipant.getDays(); ++i) {
+            let startIndex = dayStartEnd[i - 1][0]
+            let endIndex = dayStartEnd[i - 1][1]
+            for(let j = startIndex + 1; j < endIndex; ++j) {
+                let currentColumn = this.data.columns[j]
+                answerArray[i - 1].push(this.data[currentColumn].values[0])
+                columnArray[i - 1].push(currentColumn)
+            }
+        }
+
+        // calculate daily percent
+        for(let i = 0; i < SurveyParticipant.getDays(); ++i) {
+            let answersToday = answerArray[i]
+            let columnsToday = columnArray[i]
+            let rawArray = answersToday.map((_, index) => {
+                return this.isCompleted(answersToday[index], columnsToday[index])
+            })
+        }
     }
+
+    /**
+     * Determines if a field is completed based on the provided answer and field name.
+     *
+     * @param {any} ans - The answer provided for the field.
+     * @param {string} field - The name of the field.
+     * @return {number} Returns 0 if the field is not checked, 1 if the field is filled, and 2 if the field is not filled.
+     */
+    isCompleted(ans, field) {
+        // 0 is not checked, 1 is filled, 2 is not filled
+        if (field.includes("___")) {
+            let fieldName = field.split("___")[0]
+            // if field is branched and met, OR if field does not require branch
+            if((this.dataDict.isBranched(fieldName) && this.#branchMet(fieldName)) || !this.dataDict.isBranched(fieldName)) {
+                let dictPossVals = this.dataDict.getAnswers(fieldName)
+                dictPossVals = Object.keys(dictPossVals)
+                if(field !== `${fieldName}___${Math.max(...dictPossVals)}`) {
+                    return 0
+                } else { // check if any of field name has an answer
+                    for(let i = 0; i < dictPossVals.length; ++i) {
+                        if(this.data[`${fieldName}___${dictPossVals[i]}`].values[0] === 1) {
+                            // found an answer that was filled
+                            return 1
+                        }
+                    }
+                    return 2
+                }
+            }
+        
+        } else {
+            let fieldName = field
+            if (this.dataDict.isHidden(fieldName)) {
+                return 0
+            }
+            if ((this.dataDict.isBranched(fieldName) && this.#branchMet(fieldName)) || !this.dataDict.isBranched(fieldName)) {
+                if(ans === "") {
+                    return 2
+                } else {
+                    return 1
+                }
+            }
+        }
+        return 0
+    }
+
 
     #branchMet(varName) {
         let dictRef = this.dataDict.branchConditions(varName)
         for (let key in dictRef) {
+            if (key.includes("(")) {
+                continue;
+            }
             if (parseInt(this.data[key].values[0]) === parseInt(dictRef[key])) {
                 return true
             }
