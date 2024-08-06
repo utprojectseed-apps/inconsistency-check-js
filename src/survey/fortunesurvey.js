bfe0668377c91400c7f6eb29052c049a1830f373
Viet-L <vietle542521@gmail.com>
2024-08-06 16:14:42 -0500

    extract abstract class

--- src/survey/fortunesurvey.js
+++ src/survey/fortunesurvey.js
@@ -1,47 +1,13 @@
 import SurveyParticipant from './surveyparticipant';
 import DataDict from './dataDict';
+import Survey from './survey';
+import FortuneForm from './FortuneDataDict.csv'
 
 
-export default class FortuneSurvey {
-
-    constructor() {
-        this.data = undefined
-        this.participants = []
-        this.selectedIds = []
-        this.loadedIds = []
-    }
-
-    static stripTags(text) {
-        return text.replace(/<[^>]*>/g, '')
-    }
-
-    setData(data) {
-        this.data = data
-    }
-
-    setSelectedIds(ids) {
-        this.selectedIds = ids
-        for(let i = 0; i < ids.length; ++i) {
-            if(!this.loadedIds.includes(ids[i])) {
-                this.loadedIds.push(ids[i])
-                this.participants.push(new SurveyParticipant(this.data.loc({ rows: this.data["participant_id"].eq(ids[i])}), this.dataDict))
-            }
-        }
-    }
-
-    getParticipantIds() {
-        if (this.data === undefined) {
-            return []
-        }
-        return this.data['participant_id'].values
-    }
-
-    getParticipants() {
-        return this.participants
-    }
+export default class FortuneSurvey extends Survey{
 
     async readFortuneCSV() {
         this.dataDict = new DataDict()
-        await this.dataDict.readFortuneCSV()
+        await this.dataDict.readDataDict(FortuneForm)
     }
 }
